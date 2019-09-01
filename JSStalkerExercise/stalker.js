7b9551ef63156e3251746c1282d99a3eb01833c6
EC2 Default User <JasonHassold@gmail.com>
2019-09-01 20:56:39 +0000

    JS Stalker Exercise

---
+++ JSStalkerExercise/stalker.js
@@ -0,0 +1,6 @@
+var first = prompt("What is your first name?");
+var last  = prompt("What is your last name?");
+var age   = prompt("What is your age?");
+
+console.log("Hello " + first + " " + last + "!");
+console.log("How does is feel to be " + age + "?");
