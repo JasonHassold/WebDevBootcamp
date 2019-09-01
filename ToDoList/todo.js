3d0cfbf76e33c470565b9350d54a0cc6654d7743
JasonHassold <JasonHassold@gmail.com>
2019-09-01 22:26:25 +0000

    ToDo List Exercise Update

--- ToDoList/todo.js
+++ ToDoList/todo.js
@@ -8,7 +8,14 @@ while (command !== "quit") {
         todos.push(prompt("What is the new todo?"));
     }
     else if (command === "list") {
-        console.log(todos);
+        console.log("**********");
+        todos.forEach(function(todo, i){
+            console.log(i + ": " + todo);
+        });
+        console.log("**********");
+    }
+    else if (command === "delete") {
+        todos.splice(Number(prompt("Enter index of todo to delete")), 1);
     }
 }
 
